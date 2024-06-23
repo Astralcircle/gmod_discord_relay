b52b915e2d9aba987151b77e1c3e76a1a330cb4b
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2024-06-23 07:28:55 +0300

    good

--- lua/relay/sv_msgGet.lua
+++ lua/relay/sv_msgGet.lua
@@ -55,7 +55,7 @@ function socket:onMessage( txt )
               return
             end
             if resp.d.referenced_message then
-                resp.d.content = '[упомянул @' .. resp.d.referenced_message.author.username .. '] ' .. resp.d.content
+                resp.d.content = '[упомянул @' .. resp.d.referenced_message.author.global_name .. '] ' .. resp.d.content
             end
             if resp.d.attachments then 
             
@@ -68,7 +68,7 @@ function socket:onMessage( txt )
             end
             
             broadcastMsg({
-                [ 'author' ] = resp.d.author.username,
+                [ 'author' ] = resp.d.author.global_name,
                 [ 'content' ] = resp.d.content
             })
         end
