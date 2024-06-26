36a5e333a7124c1edb3a172f6e739e347166e31b
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2024-06-26 12:49:14 +0300

    Update cl_msgReceive.lua

--- lua/relay/cl_msgReceive.lua
+++ lua/relay/cl_msgReceive.lua
@@ -1,5 +1,5 @@
 net.Receive("!!discord-receive", function()
 	local msg = net.ReadTable()
 
-	chat.AddText( Discord.prefixClr, "["..Discord.prefix.."] ", Color(255, 255, 255), msg.author..": ", msg.content )
+	chat.AddText( Discord.prefixClr, "["..Discord.prefix.."] ", Color(255, 255, 100), msg.author, Color(255, 255, 255), ": ", msg.content )
 end)
